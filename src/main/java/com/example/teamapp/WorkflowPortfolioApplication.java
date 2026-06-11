7164ac26af5a120fc8936a787ffa99be34f6f11d
macha-irony <harotore1kunnrenn@gmail.com>
2026-06-11 09:44:45 +0900

    Initial commit

---
+++ src/main/java/com/example/teamapp/WorkflowPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowPortfolioApplication.class, args);
+	}
+
+}
