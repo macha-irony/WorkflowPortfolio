c8d5e324aaa596d25227850e0499cd451301340c
macha-irony <harotore1kunnrenn@gmail.com>
2026-06-23 14:37:27 +0900

    承認処理と最終承認判定を実装

--- src/main/java/com/example/workflowportfolio/approval/controller/ApprovalController.java
+++ src/main/java/com/example/workflowportfolio/approval/controller/ApprovalController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.workflowportfolio.approval.service.ApprovalService;
 
@@ -38,5 +40,15 @@ public class ApprovalController {
 		
 		model.addAttribute("approvalDetail", approvalService.getDetail(leaveId, approverId));
 		return "employee/approval/detail";
+	}
+	
+	@PostMapping("/{leaveId}/approve")
+	public String approve(@PathVariable Long leaveId,
+							RedirectAttributes redirectAttributes) {
+		//実装まで固定値
+				Long approverId = 1L;
+		approvalService.approve(leaveId, approverId);
+		redirectAttributes.addFlashAttribute("message", "申請を承認しました。");
+		return "redirect:/employee/approval/waiting";
 	}
 }
